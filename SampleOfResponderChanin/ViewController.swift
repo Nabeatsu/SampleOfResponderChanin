e53b3dfb5a74f28fb0b0a1490cf44cf9c8c6ac87
tanabenobuyuki <tanabe.1478@gmail.com>
2018-12-06 12:49:33 +0900

    Initial Commit

---
+++ SampleOfResponderChanin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleOfResponderChanin
+//
+//  Created by 田辺信之 on 2018/12/06.
+//  Copyright © 2018年 田辺信之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
